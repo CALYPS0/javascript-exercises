babea7e2f7a0b5222592cd43e838924b76b7f907
CALYPS0 <kongwaralph@yahoo.com>
2022-06-14 18:53:36 -0400

    Did exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (
+    a < 0 ||
+    b < 0 ||
+    isNaN(a) ||
+    isNaN(b) ||
+    typeof a == "string" ||
+    typeof b == "string"
+  ) {
+    return "ERROR";
+  } else if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  } else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
